414a63a903364149377744975f5aa2ca184763e8
fthhdmr <fthhdmr@gmail.com>
2020-12-16 01:41:48 +0300

    first commit

---
+++ src/views/Detail.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container">
+        <div class="detail-cart">
+            <img src="http://placehold.it/800x389" :alt="detay.title">
+            <h2>{{ detay.title }}</h2>
+            <h3>{{ detay.snippet }}</h3>
+            <p>{{ detay.body }}</p>
+
+            <!-- <pre> {{detay}} </pre> -->
+         </div> 
+    </div>
+</template>
+
+<script>
+import Axios from 'axios'
+export default {
+    data() {
+        return {
+            detay: {},
+        }
+    },
+    created() {
+        const id = this.$route.params.id;
+         Axios.get(`http://sandbox.arabamd.com/news/${id}`)
+        .then((response) => {
+            this.detay = response.data;
+        })
+    },
+}
+</script>
+
+<style lang="scss">
+.detail-cart {
+    margin-top: 50px;
+    margin-bottom: 50px;
+    img {
+        width: 100%;
+        height: auto;
+        margin-bottom: 50px;
+    }
+    h2 {
+        font-size: 2rem;
+        left: 20px;
+    }
+    h3 {
+        font-size: 1.5rem;
+        font-weight: 400;
+        left: 20px;
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,134 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <!--<pre> {{data}} </pre>-->
+      <!-- Önemli Haber -->
+      <div class="row">
+        <div v-for="news in featuredNews" v-bind:key="news.id" class="cart">
+          <img class="content-img" src="http://placehold.it/800x389" :alt="news.title" />
+          <div class="content col-lg-12">
+            <h2>{{ news.title }}</h2>
+            <p>{{ news.snippet }}</p>
+            <router-link class="btn-link" :to="{name: 'detail', params: {id: news.id}}">Detaya Git</router-link>
+          </div>
+        </div>
+      </div>
+      <!-- Daha Az Önemsiz Haber -->
+      <div class="row">
+        <div v-for="news in nonFeaturedNews" v-bind:key="news.id" class="cart-2">
+          <img class="content-img" src="http://placehold.it/800x389" :alt="news.title" />
+          <div class="content col-lg-12">
+            <h2>{{ news.title }}</h2>
+            <p>{{ news.snippet }}</p>
+            <router-link class="btn-link" :to="{name: 'detail', params: {id: news.id}}">Detaya Git</router-link>
+          </div>
+        </div>
+      </div>  
+      <div>
+    </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    
+  },
+  data() {
+    return {
+      data: [],
+    };
+  },
+  methods: {
+    detayGit(id) {
+      this.$router.push({
+        name: "detail",
+        params: {
+          id: id
+        }
+      });
+    },
+    getItem() {
+      axios.get("http://sandbox.arabamd.com/news").then((response) => {
+        this.data = response.data;
+      });
+    },
+  },
+  created() {
+    this.getItem();
+  },
+  computed: {
+    featuredNews() {
+      return this.data.filter(news => news.isFeatured)
+    },
+    nonFeaturedNews() {
+      return this.data.filter(news => !news.isFeatured)
+    }
   }
 };
 </script>
+
+
+<style lang="scss">
+  .home {
+    .cart {
+      color: white;
+      background: black;
+      margin-top: 50px;
+      margin-bottom: 50px;
+      .btn-link {
+          position: relative;
+          font-weight: 700;
+          color: white;
+          text-decoration: underline;
+        }
+      img {
+        width: 100%;
+        height: auto;
+      }
+      .content {
+        padding: 20px 20px;
+        h2 {
+        font-size: 2rem;
+        left: 20px;
+        }
+        p {
+          font-size: 1.2rem;
+          font-weight: 300;
+          left: 20px;
+        }
+      }
+    }
+    .cart-2 {
+      color: white;
+      background: black;
+      margin-top: 50px;
+      margin-bottom: 50px;
+      .btn-link {
+          position: relative;
+          font-weight: 700;
+          color: white;
+          text-decoration: underline;
+        }
+      img {
+        width: 100%;
+        height: auto;
+      }
+      .content {
+        padding: 20px 20px;
+        h2 {
+        font-size: 2rem;
+        left: 20px;
+        }
+        p {
+          font-size: 1.2rem;
+          font-weight: 300;
+          left: 20px;
+        }
+      }
+    }
+  }
+</style>
